168ce082c927529f3533308dc26cd99da8939344
sungavas <81095214+sungavas@users.noreply.github.com>
2026-05-24 23:18:00 +1200

    Update index.ts

--- src/pages/SongPages/IntroductionPage/index.ts
+++ src/pages/SongPages/IntroductionPage/index.ts
@@ -1,3 +1,3 @@
 // Author: Sungava
-export { default } from './index.svelte';
-
+// CHANGE THIS: Match your capital letters and full filename exactly!
+export { default } from './IntroductionPage.svelte';
